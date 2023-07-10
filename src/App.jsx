84ed064bb2a6558d6052e6d49216b4f597473f60
davesocool <ditchoudave18@gmail.com>
2023-07-10 17:22:43 +0000

    Main jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+
 function App() {
-  return <span>Homyz</span>;
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+    </div>
+  );
 }
 
 export default App;
